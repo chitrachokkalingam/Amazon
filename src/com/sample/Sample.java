6c0868334bc48edccb23b1d0f33ff0243c2cde15
chitrachokkalingam <chitra.lingam05@gmail.com>
2024-08-06 18:43:20 +0530

    dev

---
+++ src/com/sample/Sample.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+}
+}
